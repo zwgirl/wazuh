68e96c032e237f0caaed8a5f00736a3c25fe364d
dfolcha <daniel.folch@wazuh.com>
2020-09-17 13:35:18 +0200

    Improve wazuhctl

--- src/win32/wazuhctl.ps1
+++ src/win32/wazuhctl.ps1
@@ -16,36 +16,39 @@ param (
 )
 
 [string]${basedir} = (split-path -parent $MyInvocation.MyCommand.Definition)
-[string]${ossecconf} = ${basedir} + "ossec.conf"
+[string]${ossecconf} = ${basedir} + "\ossec.conf"
 
-${xml_ossecconf} = [xml](Get-Content -Path ${ossecconf})
+${xml_conf} = [xml](Get-Content -Path ${ossecconf})
 
 
 if (${address}) {    
     # Remove old server configuration
-    (${xml_ossecconf}.ossec_config.client.SelectNodes("server")) | ForEach-Object {
+    (${xml_conf}.ossec_config.client.SelectNodes("server")) | ForEach-Object {
         [void]$_.ParentNode.RemoveChild($_)
     }
 
     # Set new server configuration
+    $client = ${xml_conf}.ossec_config.SelectSingleNode("client")
     ${address} | ForEach-Object {
-        $server_element = ${xml_ossecconf}.CreateElement("server")
-        $client = ${xml_ossecconf}.ossec_config.SelectSingleNode("client")
+        $server_element = ${xml_conf}.CreateElement("server")
         $client.AppendChild($server_element)
     }
 }
 
 $i = 0
-${port}.GetType()
-(${xml_ossecconf}.ossec_config.client.SelectNodes("server")) | ForEach-Object {
-    $address_element = ${xml_ossecconf}.CreateElement("address")
-    $port_element = ${xml_ossecconf}.CreateElement("port")
-    $protocol_element = ${xml_ossecconf}.CreateElement("protocol")
+(${xml_conf}.ossec_config.client.SelectNodes("server")) | ForEach-Object {
+    $address_element = ${xml_conf}.CreateElement("address")
+    $port_element = ${xml_conf}.CreateElement("port")
+    $protocol_element = ${xml_conf}.CreateElement("protocol")
+    $keep_alive_element = ${xml_conf}.CreateElement("notify_time")
+    $time_reconnect_element = ${xml_conf}.CreateElement("time-reconnect")
     
     if (${address}) { 
         [void]$_.AppendChild($address_element)
         [void]$_.AppendChild($port_element)
         [void]$_.AppendChild($protocol_element)
+        [void]$_.AppendChild($keep_alive_element)
+        [void]$_.AppendChild($time_reconnect_element)
         $_.address = ${address}[$i] 
     }
 
@@ -55,65 +58,75 @@ ${port}.GetType()
     if (${protocol}) { $_.protocol = ${protocol} }
     elseif (${address}) { $_.protocol = "tcp" }
 
+    if (${protocol}) { $_.protocol = ${protocol} }
+    elseif (${address}) { $_.protocol = "tcp" }
+
+    if (${reconnection-time}) { $_."time-reconnect" = ${reconnection-time} }
+    elseif (${address}) { $_."time-reconnect" = "60" }
+
+    if (${keep-alive}) { $_.notify_time = ${keep-alive} }
+    elseif (${address}) { $_.notify_time = "10" }
+    
     $i=$i+1
 }
 
-if (${registration-address} || ${registration-port} || ${token} || ${registration-ca} || ${registration-certificate}
-    || ${registration-key} || ${name} || ${group}){
+if (${registration-address} -or ${registration-port} -or ${token} -or 
+    ${registration-ca} -or ${registration-certificate} -or 
+    ${registration-key} -or ${name} -or ${group}){
     # Remove old autoenrolment configuration
-    (${xml_ossecconf}.ossec_config.client.SelectNodes("enrollment")) | ForEach-Object {
+    (${xml_conf}.ossec_config.client.SelectNodes("enrollment")) | ForEach-Object {
         [void]$_.ParentNode.RemoveChild($_)
     }
 
-    $enrollment_element = ${xml_ossecconf}.CreateElement("enrollment")
-    $client = ${xml_ossecconf}.ossec_config.SelectSingleNode("client")
-    $client.AppendChild($server_element)
-    $enrollment = $client.enrollment
+    $enrollment_element = ${xml_conf}.CreateElement("enrollment")
+    $client = ${xml_conf}.ossec_config.SelectSingleNode("client")
+    $client.AppendChild($enrollment_element)
+    $enrollment = $client.SelectSingleNode("enrollment")
 
-    $enabled = ${xml_ossecconf}.CreateElement("enabled")
+    $enabled = ${xml_conf}.CreateElement("enabled")
     $enrollment.AppendChild($enabled)
+    $enrollment.enabled = "yes"
     
     if (${registration-address}){
-        $element = ${xml_ossecconf}.CreateElement("manager_address")
+        $element = ${xml_conf}.CreateElement("manager_address")
         $enrollment.AppendChild($element)
+        $enrollment.manager_address = ${registration-address}
     }
     if (${registration-port}){
-        $element = ${xml_ossecconf}.CreateElement("port")
+        $element = ${xml_conf}.CreateElement("port")
         $enrollment.AppendChild($element)
+        $enrollment.port = ${registration-port}
     }
     if (${token}){
-        $element = ${xml_ossecconf}.CreateElement("authorization_pass")
+        $element = ${xml_conf}.CreateElement("authorization_pass")
         $enrollment.AppendChild($element)
+        $enrollment.authorization_pass = ${token}
     }
     if (${registration-ca}){
-        $element = ${xml_ossecconf}.CreateElement("authorization_pass")
+        $element = ${xml_conf}.CreateElement("authorization_pass")
         $enrollment.AppendChild($element)
+        $enrollment.authorization_pass = ${registration-ca}
     }
     if (${registration-certificate}){
-        $element = ${xml_ossecconf}.CreateElement("server_ca_path")
+        $element = ${xml_conf}.CreateElement("server_ca_path")
         $enrollment.AppendChild($element)
+        $enrollment.server_ca_path = ${registration-certificate}
     }
     if (${registration-key}){
-        $element = ${xml_ossecconf}.CreateElement("agent_key_path")
+        $element = ${xml_conf}.CreateElement("agent_key_path")
         $enrollment.AppendChild($element)
+        $enrollment.agent_key_path = ${registration-key}
     }
     if (${name}){
-        $element = ${xml_ossecconf}.CreateElement("agent_name")
+        $element = ${xml_conf}.CreateElement("agent_name")
         $enrollment.AppendChild($element)
+        $enrollment.agent_name = ${rname}
     }
     if (${group}){
-        $element = ${xml_ossecconf}.CreateElement("groups")
+        $element = ${xml_conf}.CreateElement("groups")
         $enrollment.AppendChild($element)
+        $enrollment.groups = ${group}
     }
 }
 
-if (${keep-alive}){
-    $reg_port = ${xml_ossecconf}.CreateElement("port")
-    $enrollment.AppendChild($reg_port)
-}
-if (${reconnection-time}){
-    $reg_port = ${xml_ossecconf}.CreateElement("port")
-    $enrollment.AppendChild($reg_port)
-}
-
-${xml_ossecconf}.Save(${ossecconf})
+${xml_conf}.Save(${ossecconf})
